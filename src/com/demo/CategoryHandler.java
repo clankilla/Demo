8e6db17ed3e34457fce970bad16ea880a3619c7c
Enrique Garcia <enrique_clank@live.com>
2013-06-27 15:35:52 -0500

    update renderizaje completo del tree tag

--- src/com/demo/CategoryHandler.java
+++ src/com/demo/CategoryHandler.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -13,10 +14,28 @@ import javax.sql.DataSource;
 
 
 public class CategoryHandler {
-	ArrayList<Categories> categoryInfo = new ArrayList<>();	
+	private boolean found = false;
+	ArrayList<Categories> categoryInfo = new ArrayList<>();
 	
-	public boolean searchAndAdd(ResultSet rs) throws SQLException{
-		boolean found = false;
+	public void searchInDepthAndAdd(ResultSet rs,
+											Categories categories) throws SQLException{
+
+		if(categories.getList().size() > 0){
+			List<Categories> nodeList = categories.getList();
+			
+			for(int i = 0; i < nodeList.size(); i++){
+				if(nodeList.get(i).getCategoryName()
+										.equals(rs.getString("category_parent_node"))){
+					nodeList.get(i).addList(new Categories(rs.getString("category_name")));
+					found = true;
+					break;
+				}else
+					searchInDepthAndAdd(rs, nodeList.get(i));
+			}
+		}
+	}
+	
+	public void searchAndAdd(ResultSet rs) throws SQLException{
 		
 		for(int i = 0; i < categoryInfo.size(); i++){
 			if(categoryInfo.get(i).getCategoryName().
@@ -24,25 +43,24 @@ public class CategoryHandler {
 				categoryInfo.get(i).addList(new Categories(rs.getString("category_name")));
 				found = true;
 				break;
-			}
+			}else
+				searchInDepthAndAdd(rs, categoryInfo.get(i));
 		}
-		return found;
 	}
 	
 	public boolean search(ResultSet rs) throws SQLException{
-		boolean found = false;
+		boolean doExist = false;
 		
 		for(int i = 0; i < categoryInfo.size(); i++){
 			if(categoryInfo.get(i).getCategoryName().
 					equals(rs.getString("category_name"))){
-				found = true;
+				doExist = true;
 				break;
 			}
 		}
 		
-		return found;
+		return doExist;
 	}
-	
 	
 	public void getCategoryDB() throws SQLException{
 		DataSource ds = null;	
@@ -60,26 +78,29 @@ public class CategoryHandler {
 				throw new SQLException("Error, could not establish connection with database.");
 			
 			PreparedStatement ps = 
-					con.prepareStatement("SELECT category_name, category_parent_node FROM category_info");
+					con.prepareStatement("SELECT category_name, category_parent_node, isRoot FROM category_info");
 			
 			ResultSet rs = ps.executeQuery();
 			
 			while(rs.next()){
-				if(rs.getString("category_parent_node").equals("")){
+				if(rs.getBoolean("isRoot")){
 					if(!search(rs))
 						categoryInfo.add(new Categories(rs.getString("category_name")));
 				}else{
-					 if(!searchAndAdd(rs)){
-						categoryInfo.add(new Categories(rs.getString("category_parent_node")));
-						categoryInfo.get(categoryInfo.size()-1)
+					searchAndAdd(rs);
+					 if(!found){
+						 if(rs.getBoolean("isRoot")){
+						 	categoryInfo.add(new Categories(rs.getString("category_parent_node")));
+						 	categoryInfo.get(categoryInfo.size()-1)
 									.addList(new Categories(rs.getString("category_name")));
-					 }
+						 }
+					 }else
+					 	found = false;
 				}
 					
 			}
 		}catch(NamingException e){
 			e.printStackTrace();
 		}
-		
 	}
 }
--- src/com/demo/CategoryTag.java
+++ src/com/demo/CategoryTag.java
@@ -12,18 +12,55 @@ import javax.faces.context.ResponseWriter;
 
 @FacesComponent("CategoryTag")
 public class CategoryTag  extends UIComponentBase{
-	private String value;
+	public CategoryHandler catH;
+	private String ulStyle = "", liStyle = "";
 	public static final String COMPONENT_TYPE = "CategoryTag";
+	
+	public void doAction(Object function){
+		
+	}
+	
+	public void setUlStyle(String ulStyle){
+		this.ulStyle = ulStyle;
+	}
+	
+	public String getUlStyle(){
+		return ulStyle;
+	}
+	
+	public void setLiStyle(String liStyle){
+		this.liStyle = liStyle;
+	}
 	
 	@Override
 	public String getFamily() {
 		return COMPONENT_TYPE;
 	}
 	
+	public void renderNodes(ResponseWriter writer, Categories categories) throws IOException{
+		List<Categories> nodeList = categories.getList();
+		
+		if(nodeList.size() > 0){
+			writer.startElement("ul", this);
+			writer.writeAttribute("class", ulStyle, null);
+				for(int i = 0; i < nodeList.size(); i++){
+					writer.startElement("li", this);
+						writer.writeAttribute("class", liStyle, null);
+							writer.startElement("a", this);							
+								writer.write(nodeList.get(i).getCategoryName());
+							writer.endElement("a");
+							renderNodes(writer, nodeList.get(i));
+					writer.endElement("li");
+				}
+			writer.endElement("ul");
+		}
+	}
+	
 	public void encodeBegin(FacesContext context) throws IOException{
-		CategoryHandler catH = new CategoryHandler();
+		catH = new CategoryHandler();
 		ResponseWriter writer = context.getResponseWriter();
 		
+
 		try {
 			catH.getCategoryDB();
 		} catch (SQLException e) {
@@ -32,23 +69,15 @@ public class CategoryTag  extends UIComponentBase{
 		}
 		
 		writer.startElement("ul", this);
+		writer.writeAttribute("class", ulStyle, null);
 		for(int i = 0; i < catH.categoryInfo.size(); i++){
-			writer.startElement("li", this);
-			writer.write(catH.categoryInfo.get(i).getCategoryName());
-			writer.endElement("li");
-			
-			List<Categories> subcategory = catH.categoryInfo.get(i).getList();
-			if(subcategory.size() > 0){
 				writer.startElement("li", this);
-				writer.startElement("ul", this);
-				for(int j = 0; j < subcategory.size(); j++){
-					writer.startElement("li", this);
-					writer.write(subcategory.get(j).getCategoryName());
-					writer.endElement("li");
-				}
-				writer.endElement("ul");
+				writer.writeAttribute("class", liStyle, null);
+					writer.startElement("a", this);
+					writer.write(catH.categoryInfo.get(i).getCategoryName());
+					writer.endElement("a");
+					renderNodes(writer, catH.categoryInfo.get(i));
 				writer.endElement("li");
-			}
 		}
 		writer.endElement("ul");
 	}
